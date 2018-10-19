66feacea7fc635bb3332fbcd2221b4110dfed0b3
Dillon Ward <Dillonward13@gmail.com>
2018-10-19 17:09:13 +0100

    thread with a lock added

--- threading-sample.py
+++ threading-sample.py
@@ -1,9 +1,12 @@
-from threading import Thread
+import threading
 import time
+
+tLock = threading.Lock()
 
 def timer(name, delay, repeat):
     print('Timer: ' + name + ' Started')
-
+    tLock.acquire()
+    print(name + ' has acquired the lock')
     while repeat > 0:
         # tell the timer to wait for a certain amount of time
         time.sleep(delay)
@@ -11,12 +14,14 @@ def timer(name, delay, repeat):
 
         # decrement the value - when the value is at 0 the function is complete
         repeat -= 1
+        print(name+ ' is releasing the lock')
+        tLock.release()
     print("Timer: " + name + " Completed")
 
 def Main():
     # creates 2 threads - the target is the 'timer' class which takes in the parameters name, delay, and how many times to repeat
-    t1 = Thread(target=timer, args=("Timer1", 1, 5))
-    t2 = Thread(target=timer, args=("Timer2", 2, 5))
+    t1 = threading.Thread(target=timer, args=("Timer1", 1, 5))
+    t2 = threading.Thread(target=timer, args=("Timer2", 2, 5))
     
     # start the threads
     t1.start()
